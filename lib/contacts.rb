7bc61ffd17569351029aa855359d419a4865bf17
Andrew Joshowitz <ajoshow@sas.upenn.edu>
2018-02-09 15:08:41 +0000

    automatically backed up by learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -18,5 +18,6 @@ def remove_strawberry(contacts)
   contacts.each do |person, data|
     data.each do |attribute, value|
       if attribute == :favorite_ice_cream_flavors
-        
+        value.each do |flavor|
+          
 end
